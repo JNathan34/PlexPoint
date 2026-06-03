e939390b04cd42fcb42061f7663bcc45569b203e
Jacob <jacobnathan1718@gmail.com>
2026-06-03 23:06:36 +0100

    d

--- sw.js
+++
@@ -1,9 +0,0 @@
-/* Minimal service worker so the site is installable as a web app (PWA). */
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
